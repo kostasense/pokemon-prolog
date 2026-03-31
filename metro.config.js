10245c9d98d43c3ebf8ce32a52c396b5f0b70d89
Angel Vizcaino <leonard_viz@hotmail.com>
2026-03-30 21:44:50 -0700

    metro config clean

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Permite importar archivos .pl como assets de texto
-config.resolver.assetExts.push('pl');
-
-module.exports = config;
