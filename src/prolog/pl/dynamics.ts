9d05d1f17f9ee79989fbd160121718266ec0409b
codinavia <olivas.grecia29@gmail.com>
2026-04-07 00:16:34 -0700

    battle section rework + evolutions

--- src/prolog/pl/dynamics.ts
+++ src/prolog/pl/dynamics.ts
@@ -1,19 +1,21 @@
 export const dynamics = `
     % ==== POKEMON ====
-    :- dynamic nextTag/1.
-    :- dynamic owned/9.
-    :- dynamic enemy/6.
+    % :- dynamic nextTag/1.
+    % :- dynamic owned/9.
+    % :- dynamic enemy/6.
 
     % ==== PLAYER ====
-    :- dynamic backpack/3.
-    :- dynamic location/2.
-    :- dynamic activePokemon/1.
-    :- dynamic playerEggs/3.
+    % :- dynamic backpack/3.
+    % :- dynamic location/2.
+    % :- dynamic activePokemon/1.
+    % :- dynamic playerEggs/3.
 
     :- retractall(backpack(_, _, _)), asserta(location(littleroot, square)).
     :- retractall(backpack(_, _, _)), asserta(backpack(0, [], [])).
     :- retractall(activePokemon(_)), asserta(activePokemon(none)).
     :- retractall(playerEggs(_, _, _)), asserta(playerEggs(none, none, none)).
+    :- retractall(ownedEvolutions(_, _)), asserta(ownedEvolutions(none, [])).
+    :- retractall(traveling(_, _)), asserta(traveling(none, none)).
 
     :- retractall(nextTag(_)), asserta(nextTag(1)).
     :- retractall(owned(_, _, _, _, _, _, _, _, _)), asserta(owned(none, none, none, none, none, none, none, none, none)).
