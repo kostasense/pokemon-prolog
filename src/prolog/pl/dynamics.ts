118d11d68781649289ae9f776517c5c1598183f0
codinavia <olivas.grecia29@gmail.com>
2026-04-08 22:22:13 -0700

    finished encounters

--- src/prolog/pl/dynamics.ts
+++ src/prolog/pl/dynamics.ts
@@ -1,17 +1,6 @@
 export const dynamics = `
-    % ==== POKEMON ====
-    %:- dynamic nextTag/1.
-    %:- dynamic owned/9.
-    %:- dynamic enemy/6.
-
-    % ==== PLAYER ====
-    %:- dynamic backpack/3.
-    %:- dynamic location/2.
-    %:- dynamic activePokemon/1.
-    %:- dynamic playerEggs/3.
-
     init_game:-
-        retractall(backpack(_, _, _)), asserta(backpack(0, [], [])),
+        retractall(backpack(_, _, _)), asserta(backpack(0, [superball, superball, normal, normal], [])),
         retractall(location(_, _)), asserta(location(littleroot, plaza)),
         retractall(activePokemon(_)), asserta(activePokemon(none)),
         retractall(playerEggs(_, _, _)), asserta(playerEggs(none, none, none)),
@@ -22,7 +11,9 @@ export const dynamics = `
         retractall(owned(_, _, _, _, _, _, _, _, _)), asserta(owned(none, none, none, none, none, none, none, none, none)),
         retractall(enemy(_, _, _, _, _, _)), asserta(enemy(none, none, none, none, none, none)), 
         retractall(ownedEvolutions(_, _)), asserta(ownedEvolutions(none, [])),
-        
+        retractall(computer(_)), asserta(computer([])),
+
         retractall(winner(_, _)), asserta(winner(none, none)),
-        retractall(hitWith(_)), asserta(hitWith(none)).
+        retractall(hitWith(_)), asserta(hitWith(none)),
+        retractall(startingHP(_)), asserta(startingHP(none)).
 `;
