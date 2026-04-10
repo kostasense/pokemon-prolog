e019708d9b8157e0de63145ca2368f95c37d2cc2
codinavia <olivas.grecia29@gmail.com>
2026-04-10 02:32:26 -0700

    gym battles done + checks for game completion

--- src/prolog/pl/dynamics.ts
+++ src/prolog/pl/dynamics.ts
@@ -1,19 +1,21 @@
 export const dynamics = `
     init_game:-
-        retractall(backpack(_, _, _, _)), asserta(backpack(100, [], [], [])),
-        retractall(location(_, _)), asserta(location(littleroot, plaza)),
-        retractall(activePokemon(_)), asserta(activePokemon(none)),
-        retractall(playerEggs(_, _, _)), asserta(playerEggs(none, none, none)),
-        retractall(inRoute(_, _)), asserta(inRoute(none, none)),
-        retractall(inBattle(_)), asserta(inBattle(no)),
+        retractall(backpack(_, _, _)),                asserta(backpack(0, [], [], [])),
+        retractall(location(_, _)),                   asserta(location(littleroot, plaza)),
+        retractall(activePokemon(_)),                 asserta(activePokemon(none)),
+        retractall(playerEggs(_, _, _)),              asserta(playerEggs(none, none, none)),
+        retractall(inRoute(_, _)),                    asserta(inRoute(none, none)),
+        retractall(inBattle(_)),                      asserta(inBattle(no)),
 
-        retractall(nextTag(_)), asserta(nextTag(1)),
+        retractall(nextTag(_)),                       asserta(nextTag(1)),
         retractall(owned(_, _, _, _, _, _, _, _, _)), asserta(owned(none, none, none, none, none, none, none, none, none)),
-        retractall(enemy(_, _, _, _, _, _)), asserta(enemy(none, none, none, none, none, none)), 
-        retractall(ownedEvolutions(_, _)), asserta(ownedEvolutions(none, [])),
-        retractall(computer(_)), asserta(computer([])),
+        retractall(enemy(_, _, _, _, _, _)),          asserta(enemy(none, none, none, none, none, none)), 
+        retractall(ownedEvolutions(_, _)),            asserta(ownedEvolutions(none, [])),
+        retractall(computer(_)),                      asserta(computer([])),
 
-        retractall(winner(_, _)), asserta(winner(none, none)),
-        retractall(hitWith(_)), asserta(hitWith(none)),
-        retractall(startingHP(_)), asserta(startingHP(none)).
+        retractall(winner(_, _)),                     asserta(winner(none, none)),
+        retractall(hitWith(_)),                       asserta(hitWith(none)),
+        retractall(startingHP(_)),                    asserta(startingHP(none)),
+        retractall(queue(_, _)),                      asserta(queue([], none)),
+        retractall(gymExp(_, _)),                     asserta(gymExp([])).
 `;
